9b5b9efd7f9c2273436777a1fd7d7d5a00ee98f6
Vincent Vandeghinste <34095525+VincentCCL@users.noreply.github.com>
2026-03-03 16:45:29 +0100

    Add files via upload

--- code/finetune_pretrained_mbart.py
+++ code/finetune_pretrained_mbart.py
@@ -290,8 +290,7 @@ def main():
         args=training_args,
         train_dataset=train_ds,
         eval_dataset=val_ds,
-        tokenizer=tokenizer,
-        data_collator=data_collator,
+       data_collator=data_collator,
         compute_metrics=compute_metrics if args.eval_metrics else None,
     )
 
