ebc85a2f25d4aa0d159f4817ed328cd3d7912b36
Vincent Vandeghinste <vincent@ccl.kuleuven.be>
2026-03-11 16:54:21 +0100

    tokenizer arg

--- code/finetune_pretrained_m2m.py
+++ code/finetune_pretrained_m2m.py
@@ -262,7 +262,7 @@ def main():
         args=train_args,
         train_dataset=train_ds,
         eval_dataset=val_ds,
-        tokenizer=tokenizer,
+        #tokenizer=tokenizer,
         data_collator=data_collator,
         compute_metrics=compute_metrics,
     )
